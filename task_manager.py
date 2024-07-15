51dc6b918ce6e97f35c23e7081fef33589fe5ac3
Will Dorling <williamdorling@hotmail.com>
2024-07-15 14:36:55 +0100

    adds functionality to create new user

--- task_manager.py
+++ task_manager.py
@@ -23,77 +23,88 @@ with open('user.txt','r+') as usernames:
         # adds username/password combination to dictionary
 
 
-username = input("Welcome: Please enter your username: ")
+username = input("Welcome! Please enter your username: ")
 while username not in username_dictionary:
     username = input("No user with this name, please enter a valid username: ")
 
 password = input("Thank you, please enter your password: ")
 while username_dictionary[username] != password:
     password = input("Incorrect password, please enter your correct password: ")
-print(f"Thank you, {username}, you are now logged in")
+print(f"Thank you, {username}, you are successfully logged in")
 
 
 
-# while True:
-#     # Present the menu to the user and 
-#     # make sure that the user input is converted to lower case.
-#     menu = input('''Select one of the following options:
-# r - register a user
-# a - add task
-# va - view all tasks
-# vm - view my tasks
-# e - exit
-# : ''').lower()
+while True:
+    # Present the menu to the user and 
+    # make sure that the user input is converted to lower case.
+    menu = input('''Select one of the following options:
+r - register a user
+a - add task
+va - view all tasks
+vm - view my tasks
+e - exit
+: ''').lower()
 
-#     if menu == 'r':
-#         pass
-#         '''This code block will add a new user to the user.txt file
-#         - You can use the following steps:
-#             - Request input of a new username
-#             - Request input of a new password
-#             - Request input of password confirmation.
-#             - Check if the new password and confirmed password are the same
-#             - If they are the same, add them to the user.txt file,
-#               otherwise present a relevant message'''
+    if menu == 'r':
+        '''This code block will add a new user to the user.txt file
+        - You can use the following steps:
+            - Request input of a new username
+            - Request input of a new password
+            - Request input of password confirmation.
+            - Check if the new password and confirmed password are the same
+            - If they are the same, add them to the user.txt file,
+              otherwise present a relevant message'''
+        new_username = input("Please choose a username: ")
+        while new_username in username_dictionary:
+            new_username = input("This username is taken, please choose another username: ")
+        new_password = input("Please enter a password: ")
+        confirm_password = input("Please confirm your password: ")
+        while new_password != confirm_password:
+            new_password = input("Passwords do not match, please enter a password: ")
+            confirm_password = input("Please confirm your password: ")
+        with open('user.txt','a') as usernames:
+            usernames.write(f"{new_username}, {new_password}\n")
+            username_dictionary[new_username] = new_password
+        print(f"\nThank you, user {new_username} created.\n")
 
-#     elif menu == 'a':
-#         pass
-#         '''This code block will allow a user to add a new task to task.txt file
-#         - You can use these steps:
-#             - Prompt a user for the following: 
-#                 - the username of the person whom the task is assigned to,
-#                 - the title of the task,
-#                 - the description of the task, and 
-#                 - the due date of the task.
-#             - Then, get the current date.
-#             - Add the data to the file task.txt
-#             - Remember to include 'No' to indicate that the task is not complete.'''
+    elif menu == 'a':
+        pass
+        '''This code block will allow a user to add a new task to task.txt file
+        - You can use these steps:
+            - Prompt a user for the following: 
+                - the username of the person whom the task is assigned to,
+                - the title of the task,
+                - the description of the task, and 
+                - the due date of the task.
+            - Then, get the current date.
+            - Add the data to the file task.txt
+            - Remember to include 'No' to indicate that the task is not complete.'''
 
-#     elif menu == 'va':
-#         pass
-#         '''This code block will read the task from task.txt file and
-#          print to the console in the format of Output 2 presented in the PDF
-#          You can do it in this way:
-#             - Read a line from the file.
-#             - Split that line where there is comma and space.
-#             - Then print the results in the format shown in the Output 2 in the PDF
-#             - It is much easier to read a file using a for loop.'''
+    elif menu == 'va':
+        pass
+        '''This code block will read the task from task.txt file and
+         print to the console in the format of Output 2 presented in the PDF
+         You can do it in this way:
+            - Read a line from the file.
+            - Split that line where there is comma and space.
+            - Then print the results in the format shown in the Output 2 in the PDF
+            - It is much easier to read a file using a for loop.'''
 
-#     elif menu == 'vm':
-#         pass
-#         '''This code block will read the task from task.txt file and
-#          print to the console in the format of Output 2 presented in the PDF
-#          You can do it in this way:
-#             - Read a line from the file
-#             - Split the line where there is comma and space.
-#             - Check if the username of the person logged in is the same as the 
-#               username you have read from the file.
-#             - If they are the same you print the task in the format of Output 2
-#               shown in the PDF '''
+    elif menu == 'vm':
+        pass
+        '''This code block will read the task from task.txt file and
+         print to the console in the format of Output 2 presented in the PDF
+         You can do it in this way:
+            - Read a line from the file
+            - Split the line where there is comma and space.
+            - Check if the username of the person logged in is the same as the 
+              username you have read from the file.
+            - If they are the same you print the task in the format of Output 2
+              shown in the PDF '''
 
-#     elif menu == 'e':
-#         print('Goodbye!!!')
-#         exit()
+    elif menu == 'e':
+        print('Goodbye!!!')
+        exit()
 
-#     else:
-#         print("You have entered an invalid input. Please try again")
+    else:
+        print("You have entered an invalid input. Please try again")
